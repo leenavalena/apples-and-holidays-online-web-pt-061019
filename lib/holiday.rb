ffdd1cc996bd1be0fc3ddee20d99c5d3f82aaba9
Valena Del Valle <valenadelvalle4@gmail.com>
2019-06-29 00:16:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,6 +42,7 @@ def add_supply_to_memorial_day(holiday_hash, supply)
     supplies << "Grill" "Table Cloth"
       binding.pry
     end
+  end
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
